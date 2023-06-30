017faf072f15bc735bc92df105f945b918b8a9a7
cs-ankit-gupta <gankit@fortinet.com>
2023-06-30 15:35:09 +0530

    added comment and update method name

--- fuzzy-search/operations.py
+++ fuzzy-search/operations.py
@@ -10,37 +10,47 @@ class Fuzzy_Search:
     def __init__(self):
         pass
 
-    def filter_data(self, json_record, params):
-        for key, value in params['targetKeyValue'].items():
-            record_value = json_record.get(key)
-            if record_value is not None:
-                ratio = self.set_ratio(
-                    str(value), record_value, params)
-                if ratio >= params['similarityThreshold']:
-                    return True
-                else:
-                    return False
+    # checks if json record contains value for provided key
+    def is_record_found(self, key, value, json_record, params):
+        record_value = json_record.get(key)
+        if record_value is not None:
+            calculated_threshold = self.calculate_threshold(
+                str(value), record_value, params)
+            if calculated_threshold >= params['similarityThreshold']:
+                return True
+            else:
+                logger.debug(str("Threshold for value {0} in {1} is {2}".format(
+                    value, json_record, calculated_threshold)))
+                return False
+        else:
+            logger.debug(
+                str("Value for key {0} in {1} is found none.".format(key, json_record)))
+            return False
 
-    def set_ratio(self, str1, str2, params):
+    # calculate threshold
+    def calculate_threshold(self, text1, text2, params):
         try:
-            if str1 in str2 or str2 in str1:
+            if text1 in text2 or text2 in text1:
                 return 100
 
             count = 0
             # Unique within string
-            modified_str1 = self.remove_special_characters(str1.lower())
-            modified_str2 = list(
-                set(self.remove_special_characters(str2.lower()).split(' ')))
-            for data in ' '.join(modified_str2).split():
-                if re.search(r'\b{0}\b'.format(data), modified_str1):
+            text1 = self.remove_special_characters(text1.lower())
+            text2 = list(
+                set(self.remove_special_characters(text2.lower()).split(' ')))
+
+            for data in ' '.join(text2).split():
+                # check boundary search
+                if re.search(r'\b{0}\b'.format(data), text1):
                     count += 1
+                # perform fuzzy search
                 elif params['fuzzyMatch']:
                     params['limit'] = 5
-                    if len(self.get_fuzzy_search_data(data, modified_str1, params)) > 0:
+                    if len(self.get_fuzzy_search_data(data, text1, params)) > 0:
                         count += 1
-            if len(modified_str2) <= len(modified_str1.split(' ')):
-                return round(count / len(modified_str2) * 100)
-            return round(count / len(modified_str1.split(' ')) * 100)
+            if len(text2) <= len(text1.split(' ')):
+                return round(count / len(text2) * 100)
+            return round(count / len(text1.split(' ')) * 100)
         except Exception as err:
             logger.exception(str(err))
             raise Exception(err)
@@ -52,17 +62,17 @@ class Fuzzy_Search:
             logger.exception(str(err))
             raise Exception(err)
 
-    def get_fuzzy_search_data(self, keyword_to_search, inputText, params):
-        return difflib.get_close_matches(keyword_to_search, inputText.split(
+    def get_fuzzy_search_data(self, keyword_to_search, input_text, params):
+        return difflib.get_close_matches(keyword_to_search, input_text.split(
             ' '), n=params['limit'], cutoff=params['cutoff']/100)
 
     def verify_threshold(self, threshold_value, key_name):
         if threshold_value in range(0, 101):
             return
         logger.exception(
-            str("{0} value should be in range 0 to 100".format(key_name)))
+            str("{0} value should be in range of 0 to 100".format(key_name)))
         raise Exception(
-            str("{0} value should be in range 0 to 100".format(key_name)))
+            str("{0} value should be in range of 0 to 100".format(key_name)))
 
 
 def filter_json_by_key_value(config, params):
@@ -74,13 +84,15 @@ def filter_json_by_key_value(config, params):
         if 'cutoff' in params.keys():
             fuzzy_search.verify_threshold(params['cutoff'], "Cut Off")
         if isinstance(params['inputJSON'], list):
-            for record in params['inputJSON']:
-                if fuzzy_search.filter_data(record, params):
-                    filter_data.append(record)
+            for json_record in params['inputJSON']:
+                for key, value in params['targetKeyValue'].items():
+                    if fuzzy_search.is_record_found(key, value, json_record, params) and json_record not in filter_data:
+                        filter_data.append(json_record)
             return filter_data
         elif isinstance(params['inputJSON'], dict):
-            if fuzzy_search.filter_data(params['inputJSON'], params):
-                filter_data.append(params['inputJSON'])
+            for key, value in params['targetKeyValue'].items():
+                if fuzzy_search.is_record_found(key, value, params['inputJSON'], params) and params['inputJSON'] not in filter_data:
+                    filter_data.append(params['inputJSON'])
             return filter_data
     except Exception as err:
         logger.exception(str(err))
